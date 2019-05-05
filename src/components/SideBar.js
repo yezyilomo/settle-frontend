ecf6a011f5e7dacb0e2e338db89abdd7dc96cbbf
yezy <yezileliilomo@hotmail.com>
2019-05-05 22:52:35 +0300

    :iphone:Restyle SideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -22,13 +22,20 @@ function SideBar(props) {
                     <label class="form-check-label mt-2 mb-0 p-0 mx-2">Price range</label>
                     <div class="row p-0 mx-2">
                         <input type="number" class="form-control m-0 px-2 col-5 d-inline" placeholder="From" />
-                        <div class="col p-0 m-0"><hr class="mx-1"/></div>
+                        <div class="col p-0 m-0"><hr class="mx-1" /></div>
                         <input type="number" class="form-control m-0 px-2 col-5 d-inline" placeholder="To" />
                     </div>
                 </div>
                 <input type="text" class="form-control my-4" id="inlineFormInput" placeholder="Location" />
-                <label class="form-check-label mt-2">Electricity</label>
-                <input class="form-check-input ml-4 my-2" type="checkbox" ></input>
+                <input type="text" class="form-control my-4" id="inlineFormInput" placeholder="Amenities" />
+                <div class="mt-4">
+                    <label class="form-check-label mt-2 ml-1">Electricity</label>
+                    <input class="form-check-input ml-4 my-2" type="checkbox" ></input>
+                </div>
+                <div class="mt-4">
+                    <label class="form-check-label mt-2 ml-1">Price Negotiation</label>
+                    <input class="form-check-input ml-4 my-2" type="checkbox" ></input>
+                </div>
                 <button type="submit" class="btn btn-info mt-4 col-12">Submit</button>
             </form>
         </div>
