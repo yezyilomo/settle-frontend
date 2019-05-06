8e349f66f2e0a76ce3332c376594d7ebf5799fd5
yezy <yezileliilomo@hotmail.com>
2019-05-06 14:28:01 +0300

    Replace BrowserRouter with HashRouter for the app to work on github page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Home} from './components'
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<Router><Home/></Router>, document.getElementById('root'));
+ReactDOM.render(<Router base="/"><Home/></Router>, document.getElementById('root'));
