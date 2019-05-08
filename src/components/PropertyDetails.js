515af40eef3ff3eb43f57e893a73d814543a58d8
yezy <yezileliilomo@hotmail.com>
2019-05-08 20:09:01 +0300

    :sparkles:Add full image view with image modal

--- src/components/PropertyDetails.js
+++ src/components/PropertyDetails.js
@@ -1,14 +1,35 @@
 import React, { useEffect } from 'react';
 import './PropertyDetails.css';
 import { Link } from 'react-router-dom';
+import { Block } from './'
 
-function PropertyImage(props) {
+
+function ImageModal(props) {
     return (
-        <div class="row pictures col-6 col-sm-6 col-md-4 col-lg-3 p-1 p-sm-2 m-0">
-            <div class="other-prop-img col-12 bg-white">
-                <img src={props.src} alt="" />
+        <div class="modal fade" id={`Modal_${props.id}`}>
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content border-0">
+                    <div class="modal-body p-0 m-0 border-0">
+                        <button class="modal-close close d-none d-lg-block" data-dismiss="modal" aria-label="Close">
+                            <span class="fa fa-times"></span>
+                        </button>
+                        <img class="full-img" src={props.src} alt="" />
+                    </div>
+                </div>
             </div>
         </div>
+    );
+}
+function PropertyImage(props) {
+    return (
+        <Block>
+            <div class="row pictures col-6 col-sm-6 col-md-4 col-lg-3 p-1 p-sm-2 m-0">
+                <div class="other-prop-img col-12 bg-white">
+                    <img class="small-img" src={props.src} alt="" data-toggle="modal" data-target={`#Modal_${props.id}`} />
+                </div>
+            </div>
+            <ImageModal src={props.src} id={props.id} />
+        </Block>
     )
 }
 
@@ -22,11 +43,12 @@ function PropertyDetails(props) {
 
     return (
         <div class="col-12 p-2 m-0">
-            <div class="property row col-12 p-0 m-0">
-                <div class="main-prop-img col-12 col-sm-6 bg-white">
-                    <img src={props.property.img} alt="" />
+            <div class="property-details row col-12 p-0 m-0">
+                <div class="main-prop-img col-12 col-sm-6">
+                    <img class="main-img" src={props.property.img} alt="" data-toggle="modal" data-target={`#Modal_${props.property.id}`} />
+                    <ImageModal src={props.property.img} id={props.property.id} />
                 </div>
-                <div class="detailed-prop-info col-12 col-sm-6 bg-white px-1 px-sm-4 px-lg-4">
+                <div class="detailed-prop-info col-12 col-sm-6 px-1 px-sm-4 px-lg-4">
                     <div class="property-type">Available for <span class="bg-info">{props.property.category}</span></div>
                     <div class="property-location"> <i class="fa fa-map-marker-alt"></i> {props.property.location}</div>
                     <div class="property-price">
@@ -50,9 +72,9 @@ function PropertyDetails(props) {
             </div>
 
             <div class="row col-12 mb-3 m-0 mt-4 mt-sm-5 p-0">
-                <h5 class="w-100 ml-2 mb-0 mt-0">Other Images</h5>
+                <h6 class="w-100 ml-2 mb-0 mt-0">Other Images</h6>
                 {props.properties.map(property =>
-                    <PropertyImage src={property.img} />
+                    <PropertyImage src={property.img} id={property.id} />
                 )}
             </div>
         </div>
