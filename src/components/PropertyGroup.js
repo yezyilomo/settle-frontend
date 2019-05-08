b8a90dfd594979d72bcf9616aa8ed6429be63a00
yezy <yezileliilomo@hotmail.com>
2019-05-08 20:04:52 +0300

    :iphone:Bold group headers

--- src/components/PropertyGroup.js
+++ src/components/PropertyGroup.js
@@ -5,7 +5,7 @@ import './PropertyGroup.css';
 function PropertyGroup(props) {
     return (
         <div class="property-container row col-12 mb-3 mt-0 mx-0 p-0">
-            <h5 class="w-100 ml-2 mb-0 mt-0">{props.header}</h5>
+            <h6 class="w-100 ml-2 mb-0 mt-0 font-weight-bold">{props.header}</h6>
             {props.properties.map(property =>
                 <PropertyOverview property={property}/>
             )}
