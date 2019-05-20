9257a4d87b7a9d8f478dfaf481b67b52363a818d
yezy <yezileliilomo@hotmail.com>
2019-05-20 22:05:17 +0300

    :art:Improve component look

--- src/components/PropertyDetails.js
+++ src/components/PropertyDetails.js
@@ -4,15 +4,15 @@ import { } from 'react-router-dom';
 import { Block } from './'
 
 
-function ImageModal(props) {
+function ImageModal(props){
     return (
-        <div class="modal fade" id={`Modal_${props.id}`}>
-            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal fade p-0 m-0" id={`Modal_${props.id}`}>
+            <button class="modal-close close" data-dismiss="modal" aria-label="Close">
+                <span class="fa fa-times"></span>
+            </button>
+            <div class="modal-dialog modal-dialog-centered mx-auto modal-lg" role="document">
                 <div class="modal-content border-0">
                     <div class="modal-body p-0 m-0 border-0">
-                        <button class="modal-close close d-none d-lg-block" data-dismiss="modal" aria-label="Close">
-                            <span class="fa fa-times"></span>
-                        </button>
                         <img class="full-img" src={props.src} alt="" />
                     </div>
                 </div>
@@ -20,7 +20,7 @@ function ImageModal(props) {
         </div>
     );
 }
-function PropertyImage(props) {
+function PropertyImage(props){
     return (
         <Block>
             <div class="row pictures col-6 col-sm-6 col-md-4 col-lg-3 p-1 p-sm-2 m-0">
@@ -33,7 +33,35 @@ function PropertyImage(props) {
     )
 }
 
-function PropertyDetails(props) {
+function shorten(str, to){
+    let long = str.slice(to);
+    if(long){
+        return str.slice(0, to) + "...";
+    }
+    return str;
+}
+
+function Badges(props){
+    return (
+        props.values && props.values.length>0?
+        <div class="mb-3">
+            <div class="h5">{props.label}</div>
+            {props.values.map((val) => {
+                return (
+                    <Block>
+                    <span class="badge badge-secondary mb-1 mr-1">
+                        <span id={val} class={`fa fa-${val} badge-close`}/>&nbsp;
+                        <span class="badge-body">{shorten(val, 17)}</span>
+                    </span>
+                    </Block>
+                );
+            })}
+        </div>:
+        null
+    )
+}
+
+function PropertyDetails(props){
     let rate = () => {
         window.scrollTo(0, 0);
         let id = `star${props.property.rating}-${props.property.id}`;
@@ -41,14 +69,27 @@ function PropertyDetails(props) {
     };
     useEffect(rate, [props.property.rating])
 
+    let otherFeatures = [
+        {name: "Electricity", value: "Available"},
+        {name: "Bathroom", value: "Self-Contained"},
+        {name: "Backup Generator", value: "Available"},
+        {name: "Water", value: "Available"},
+        {name: "Price Negotiation", value: "Yes"},
+        {name: "Roofing", value: "Gypsum"}
+    ]
+
+    let amenities = ["wifi", "shower", "phone", "car", "camera"];
+    let Services = ["fish", "video", "adjust", "desktop", "laptop"];
+    let Potentials = ["paw", "music", "podcast", "microphone", "feather-alt"];
+
     return (
         <div class="col-12 p-2 m-0">
             <div class="property-details row col-12 p-0 m-0">
-                <div class="main-prop-img col-12 col-sm-6">
+                <div class="main-prop-img col-12 col-lg-6">
                     <img class="main-img" src={props.property.img} alt="" data-toggle="modal" data-target={`#Modal_${props.property.id}`} />
                     <ImageModal src={props.property.img} id={props.property.id} />
                 </div>
-                <div class="detailed-prop-info col-12 col-sm-6 px-1 px-sm-4 px-lg-4">
+                <div class="detailed-prop-info col-12 col-sm-6 col-lg-3 px-1 px-sm-0 px-lg-4 mt-3 mt-lg-0">
                     <div class="property-type">Available for <span class="bg-info">{props.property.category}</span></div>
                     <div class="property-location"> <i class="fa fa-map-marker-alt"></i> {props.property.location}</div>
                     <div class="property-price">
@@ -66,13 +107,22 @@ function PropertyDetails(props) {
                         <label for={`star2-${props.property.id}`} title="2 star"><i class="fa fa-star"></i></label>
                         <input type="radio" id={`star1-${props.property.id}`} name={`rating${props.property.id}`} value="1" />
                         <label for={`star1-${props.property.id}`} title="1 star"><i class="fa fa-star"></i></label>
-                        <span>Rating</span>
+                        <span>Rating:</span>
                     </div>
+                    <div class="w-100"/>
+                    {otherFeatures.map((feature)=>{
+                        return <div class="other-feature"><b>{feature.name}:</b> {feature.value}</div>;
+                    })}
+                </div>
+                <div class="col-12 col-sm-6 col-lg-3 px-1 px-sm-0 px-lg-4 mt-3 mt-lg-0 text-dark">
+                    <Badges values={amenities} label="Amenities"/>
+                    <Badges values={Services} label="Nearby Services"/>
+                    <Badges values={Potentials} label="Potential For"/>
                 </div>
             </div>
 
-            <div class="row col-12 mb-3 m-0 mt-4 mt-sm-5 p-0">
-                <h6 class="w-100 ml-2 mb-0 mt-0">Other Images</h6>
+            <div class="row col-12 mb-3 m-0 mt-3 mt-sm-5 p-0 text-dark">
+                <div class="w-100 ml-1 ml-sm-2 mb-0 mt-0 h5">Other Images</div>
                 {props.properties.map(property =>
                     <PropertyImage src={property.img} id={property.id} />
                 )}
--- src/components/UploadProperty.js
+++ src/components/UploadProperty.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, {  } from 'react';
 import './UploadProperty.css';
 import { } from 'react-router-dom';
-import { Block, Select } from './';
+import { Select, FeatureForm } from './';
 
 
 function UploadProperty(props) {
@@ -9,98 +9,124 @@ function UploadProperty(props) {
         "one", "two", "three", "four", "five", "six", "seven",
         "eight", "nine", "ten", "eleven", "twelve", "thirteen",
         "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
-        "nineteen", "twelve"
+        "nineteen", "twenty"
     ]
+
+    let values = ["one", "two", "three",
+    "one", "two", "three", "four", "five", "six", "seven",
+    "eight", "nine", "ten", "eleven", "twelve", "thirteen",
+    "fourteen", "fifteen", "sixteen", "seventeen", "eighteen",
+    "nineteen", "twenty"
+]
+    let currencies = ["TZS", "USD"];
+    let countries = ["Tanzania", "Kenya", "Uganda", "Zambia", "Zanzibar"];
 
     return (
         <div class="container-fluid p-0 m-0 px-2">
             <form class="property-form text-secondary">
                 <div class="row">
-                    <div class="col-12 col-md-6">
-                        <div class="form-row justify-content-center p-0 m-0">
-                            <label class="form-check-label m-0 mt-lg-1 text-align-left col-12">Price</label>
-                            <div class="row p-0 m-0">
-                                <div class="col-8 d-inline my-1 px-1">
-                                    <input type="number" class="form-control" placeholder="Price" />
-                                </div>
-                                <div class="col-4 d-inline my-1 px-1">
-                                    <input type="text" class="form-control" placeholder="Currency" />
+                    <div class="col-12 col-md-6 justify-content-center ">
+
+                            <div class="row p-0 m-0 my-0 my-lg-1">
+                                <label class="form-check-label col-12 px-2">Pricing</label>
+                                <div class="col-12 my-1">
+                                    <div class="row">
+                                        <div class="col-8 px-2">
+                                            <input type="number" class="form-control" placeholder="Price" />
+                                        </div>
+                                        <div class="col-4 px-2">
+                                            <select class="custom-select" name="currency">
+                                                <option value="" disabled selected>Currency</option>
+                                                {currencies.map((currency)=><option value={currency}>{currency}</option>)}
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-row justify-content-center p-0 m-0 my-2 my-lg-3">
-                            <label class="form-check-label mt-2 text-align-left col-12">Location</label>
-                            <div class="row p-0 m-0">
-                                <div class="col-12 d-inline my-1 px-1">
-                                    <input type="text" class="form-control" placeholder="Country" />
+
+                            <div class="row p-0 m-0 my-2 my-lg-3">
+                                <label class="form-check-label col-12 px-2">Location</label>
+                                <div class="col-12 my-1 px-2">
+                                    <select class="custom-select" name="country">
+                                        <option value="" disabled selected>Country</option>
+                                        {countries.map((country)=><option value={country}>{country}</option>)}
+                                    </select>
                                 </div>
-                                <div class="col-6 d-inline my-1 px-1">
-                                    <input type="text" class="form-control" placeholder="Region" />
+                                <div class="col-12 my-1">
+                                    <div class="row">
+                                        <div class="col-6 px-2">
+                                            <input type="text" class="form-control" placeholder="Region" />
+                                        </div>
+                                        <div class="col-6 px-2">
+                                            <input type="text" class="form-control" placeholder="Distric" />
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-6 d-inline my-1 px-1">
-                                    <input type="text" class="form-control" placeholder="Distric" />
-                                </div>
-                                <div class="col-6 d-inline my-1 px-1">
-                                    <input type="text" class="form-control" placeholder="Street1" />
-                                </div>
-                                <div class="col-6 d-inline my-1 px-1">
-                                    <input type="text" class="form-control" placeholder="Street2" />
+                                <div class="col-12 my-1">
+                                    <div class="row">
+                                        <div class="col-6 px-2">
+                                            <input type="text" class="form-control" placeholder="Street1" />
+                                        </div>
+                                        <div class="col-6 px-2">
+                                            <input type="text" class="form-control" placeholder="Street2" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-12 px-1 my-3 my-lg-4">
-                            <label class="form-check-label col-12 px-0">Amenities</label>
-                            <Select
-                                class="custom-select"
-                                options={options}
-                                placeholder="Select Amenity"
-                            />
-                        </div>
+                            <div class="row col-12 p-0 m-0 my-2 my-lg-3">
+                                <label class="form-check-label col-12 px-2">Amenities</label>
+                                <div class="col-12 px-2">
+                                    <Select class="custom-select" options={options} placeholder="Select Amenity"/>
+                                </div>
+                            </div>
 
-                        <div class="col-12 px-1 my-3 my-lg-4">
-                            <label class="form-check-label col-12 px-0">Services</label>
-                            <Select
-                                class="custom-select"
-                                options={options}
-                                placeholder="Select Service"
-                            />
-                        </div>
+                            <div class="row col-12 p-0 m-0 my-2 my-lg-3">
+                                <label class="form-check-label col-12 px-2">Services</label>
+                                <div class="col-12 px-2">
+                                    <Select class="custom-select" options={options} values={values} placeholder="Select Service"/>
+                                </div>
+                            </div>
 
-                        <div class="col-12 px-1 my-3 my-lg-4">
-                            <label class="form-check-label col-12 px-0">Potentials</label>
-                            <Select
-                                class="custom-select"
-                                options={options}
-                                placeholder="Select Potential"
-                            />
-                        </div>
+                            <div class="row col-12 p-0 m-0 my-2 my-lg-3">
+                                <label class="form-check-label col-12 px-2">Potentials</label>
+                                <div class="col-12 px-2">
+                                    <Select class="custom-select" options={options} placeholder="Select Potential"/>
+                                </div>
+                            </div>
 
-                        <div class="col-12 px-1 my-2 my-lg-3">
-                            <input type="text" class="form-control" id="inlineFormInput" placeholder="Location" />
-                        </div>
-                        <div class="col-12 px-1 my-2 my-lg-3">
-                            <input type="text" class="form-control" id="inlineFormInput" placeholder="Amenities" />
-                        </div>
+                            <div class="row p-0 m-0 my-4">
+                                <FeatureForm  label="Add Other Features"/>
+                            </div>
+
                     </div>
 
                     <div class="col-12 col-md-6">
-                        <div class="row col-12 p-0 m-0 justify-content-center mt-3">
-                            <div class="upload-main-img">
-                                <span class="fa fa-camera"/>
+                        <div class="row p-0 m-0 mt-3 mt-md-1 justify-content-center">
+                            <label class="form-check-label col-12 p-0 m-0 px-2">Pictures</label>
+                            <div class="upload-main-img d-flex flex-column align-content-center justify-content-center flex-wrap">
+                                <div>Upload main image</div>
+                                <div class="d-flex flex-row justify-content-end">
+                                    <span class="camera fa fa-camera"/>
+                                    <span class="plus fa fa-plus"/>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="row col-12 p-0 m-0 justify-content-center mt-3">
-                            <div class="upload-other-img">
-                                <span class="fa fa-camera align-content-end"/>
+                        <hr class="mx-0 mx-lg-2"/>
+                        <div class="row p-0 m-0  mt-3 justify-content-center">
+                            <div class="upload-other-img d-flex flex-column justify-content-center align-content-center flex-wrap">
+                                <div>Upload other image</div>
+                                <div class="d-flex flex-row justify-content-center">
+                                    <span class="camera fa fa-camera"/>
+                                    <span class="plus fa fa-plus"/>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div class="row justify-content-center">
-                    <button type="submit" class="btn btn-info mt-4 col-6">Submit</button>
+                    <button type="submit" class="btn btn-info mt-4 col-11 col-sm-6">Submit</button>
                 </div>
 
             </form>
