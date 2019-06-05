bf1633363c47fc918dccf32b1fa85d8a08d050fa
yezy <yezileliilomo@hotmail.com>
2019-06-05 13:12:41 +0300

    :sparkles:Add custom hooks

---
+++ src/hooks/useFetcher.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+function useFetcher(action) {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+
+    async function loadData() {
+        try {
+            setLoading(true);
+            const actionData = await action();
+            setData(actionData);
+        } catch (e) {
+            setError(e);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [action]);
+
+    return [data, loading, error];
+}
+
+export {useFetcher}
