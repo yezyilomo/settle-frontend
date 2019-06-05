fc93679cdd5a90becb77e2bf0b5c4ace3b2ac945
yezy <yezileliilomo@hotmail.com>
2019-06-05 13:13:37 +0300

    :sparkles:Add Fetcher component for handling data fetching

---
+++ src/components/Fetcher.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import {useFetcher} from '../hooks';
+
+const Fetcher = ({ action, placeholder, children }) => {
+    const [data, loading, error] = useFetcher(action);
+
+    if (loading){
+        if(placeholder){
+            return placeholder;
+        }
+        return null;
+    }
+    if (error) return <div>Error occured while loading..</div>
+
+    if (!data) return null;
+
+    return children(data);
+};
+
+export {Fetcher};
