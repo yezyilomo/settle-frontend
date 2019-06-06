afdf4edc4539313f1dea123db1304cce6c952dcc
yezy <yezileliilomo@hotmail.com>
2019-06-06 23:36:00 +0300

    :art:Cleanup

--- src/components/Fetcher.js
+++ src/components/Fetcher.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { } from 'react';
 import {useFetcher} from '../hooks';
 
 const Fetcher = ({ action, placeholder, children }) => {
