16ad575af156d70728fbdcc80e728e54ecd1043c
yezy <yezileliilomo@hotmail.com>
2019-06-06 23:32:34 +0300

    :art:Cleanup

--- src/hooks/useFetcher.js
+++ src/hooks/useFetcher.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function useFetcher(action) {
     const [loading, setLoading] = useState(false);
