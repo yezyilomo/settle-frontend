f8d24b59aa6ab22fb1a9773871c2beb7e1231924
yezy <yezileliilomo@hotmail.com>
2019-06-21 12:29:50 +0300

    :sparkles:Implement Login logic

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,9 +1,48 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import { } from 'react-router-dom';
+import { useGlobalState } from '../hooks';
 import './LogIn.css';
-
+import {API_URL} from '../';
 
 function LogIn(props) {
+    let [ ,updateUser] = useGlobalState("User");
+    let [loginError, setLoginError] = useState('');
+
+    let updateLogin = (response) => {
+        let authToken = response.token;
+        if (authToken !== undefined){
+            var d = new Date();
+            d.setTime(d.getTime() + 30*24*60*60*1000); // in milliseconds
+            document.cookie = `auth_token=${authToken};path=/;expires=${d.toGMTString()};SameSite=Lax;`;
+            updateUser([
+                {field: "isLoggedIn", value: true},
+                {field: "authToken", value: authToken}
+            ]);
+            setLoginError("");
+            window.location = "/";
+        }
+        else if(response.non_field_errors !== undefined){
+            setLoginError("Invalid credentials, please try again!.");
+        }
+        else{
+            setLoginError("Failed to login, please try again!.");
+        }
+    }
+
+    let login = (event) => {
+        event.preventDefault();
+        let form = event.target
+        let username = form.username.value;
+        let password = form.password.value;
+        var formdata = new FormData();
+        formdata.append("username", username);
+        formdata.append("password", password);
+        let loginUrl = `${API_URL}/api/token-auth/`
+        fetch(loginUrl, {method: 'POST', body: formdata})
+        .then(response => response.json())
+        .then(res => updateLogin(res))
+        .catch(error => console.log(error));
+    }
     return (
         <div class="login-modal modal fade p-0 m-0" id="login-modal">
             <div class="modal-dialog modal-dialog-centered mx-auto modal-lg mt-0" role="document">
@@ -14,18 +53,22 @@ function LogIn(props) {
                     <div class="modal-body p-0 m-0 border-0 py-5">
                         <div class="container-fluid p-0 m-0 px-2">
                             <center class="header col-12 h4 mt-2 text-secondary">Login to Your Account</center>
-                            <form class="login-form text-secondary">
+                            <form class="login-form text-secondary" onSubmit={login}>
                                 <div class="row justify-content-center">
                                     <div class="col-10 p-0 m-0 my-2 my-lg-3">
                                         <div class="col-12 px-2">
-                                            <input type="text" name="user" class="form-control" placeholder="Username" />
+                                            <input type="text" name="username" class="form-control" placeholder="Username" />
                                         </div>
                                     </div>
 
                                     <div class="col-10 p-0 m-0 my-2 my-lg-3">
                                         <div class="col-12 px-2">
-                                            <input type="password" name="pass" class="form-control" placeholder="Password" />
+                                            <input type="password" name="password" class="form-control" placeholder="Password" />
                                         </div>
+                                    </div>
+
+                                    <div class="text-danger">
+                                        {loginError}
                                     </div>
 
                                     <div class="col-10 p-0 m-0 my-2 my-lg-3">
