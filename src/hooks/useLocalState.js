428ec9ea31e4d23f277c47c466f2d0f794182d88
yezy <yezileliilomo@hotmail.com>
2019-07-04 15:24:12 +0300

    :art:use useReducer hook only

--- src/hooks/useLocalState.js
+++ src/hooks/useLocalState.js
@@ -2,9 +2,7 @@ import { useReducer } from 'react';
 import {updateReducer} from '../reducers';
 
 function useLocalState(initialState){
-    // [state, updateState]
     return useReducer(updateReducer, initialState);
 }
-
 
 export {useLocalState}
