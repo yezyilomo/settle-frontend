96638a3088e2dc8e2a7adc29c65ce2d11cbf04f7
yezy <yezileliilomo@hotmail.com>
2019-11-04 10:14:59 +0300

    :fire:Remove unused files

--- src/hooks/useLocalState.js
+++
@@ -1,8 +0,0 @@
-import { useReducer } from 'react';
-import {updateReducer} from '../reducers';
-
-function useLocalState(initialState){
-    return useReducer(updateReducer, initialState);
-}
-
-export {useLocalState}
