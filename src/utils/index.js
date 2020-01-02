21930a8d748ea33f52a62d179af2717b396ff8e8
yezy <yezileliilomo@hotmail.com>
2020-01-03 00:26:35 +0300

    :art:Organize utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,53 @@
-import {onScrollToBottom} from './onScrollToBottom';
-import {getCookie} from './getCookie';
+function setErrorClass(query = "input, select, textarea") {
+    const inputs = document.querySelectorAll(query);
+
+    inputs.forEach(input => {
+        input.addEventListener(
+            "invalid",
+            event => {
+                input.classList.add("error");
+            },
+            false
+        );
+    });
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function onScrollToBottom(handleScrollToBottom, y = 1) {
+    let scrollToBottomEventHandler = () => {
+        let scrollTop = (
+            window.pageYOffset ||
+            document.documentElement.scrollTop ||
+            document.body.scrollTop || 0
+        );
+
+        let distanceFromBottom = (
+            document.documentElement.offsetHeight -
+            (window.innerHeight + scrollTop)
+        )
+
+        if (distanceFromBottom < y && distanceFromBottom > -1) {
+            handleScrollToBottom();
+        }
+    };
+
+    return scrollToBottomEventHandler
+}
 
 export {
-  onScrollToBottom, getCookie
+    setErrorClass, getCookie, onScrollToBottom
 }
