f58633aa7b795cf434015ec5f2f28d91895bac1b
yezy <yezileliilomo@hotmail.com>
2020-01-03 00:19:02 +0300

    :sparkles:Add Fetcher components

---
+++ src/components/Fetchers.js
@@ -0,0 +1,38 @@
+import {useLocalFetcher, useGlobalFetcher} from '../hooks';
+
+
+const LocalFetcher = ({ action, placeholder, children, error }) => {
+    const [data, updateData, loading, fetchError] = useLocalFetcher(action);
+
+    if (loading){
+        if(placeholder){
+            return placeholder;
+        }
+        return null;
+    }
+    if (fetchError) return error;
+
+    if (!data) return null;
+
+    return children(data, updateData);
+};
+
+
+const GlobalFetcher = ({ action, placeholder, children, error, selection }) => {
+    const [data, updateData, loading, fetchError] = useGlobalFetcher(action, selection);
+
+    if (loading){
+        if(placeholder){
+            return placeholder;
+        }
+        return null;
+    }
+    if (fetchError) return error;
+
+    if (!data) return null;
+
+    return children(data, updateData);
+};
+
+
+export {LocalFetcher, GlobalFetcher};
