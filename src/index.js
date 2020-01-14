f5e16930c729378e8bc269bcf45320a761d756e7
yezy <yezileliilomo@hotmail.com>
2020-01-14 08:32:48 +0300

    :alien:Update code due to API changes

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import { HashRouter as Router } from 'react-router-dom';
 import store from './store';
 
 
-//let API_URL = "http://192.168.43.129:8000/api";
-let API_URL = "http://yezyilomo.com:8001/api";
+//let API_URL = "http://192.168.43.129:8000/";
+let API_URL = "https://api.yezyilomo.com";
 
 window.onScrollActions = {}
 
