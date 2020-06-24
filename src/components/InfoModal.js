dc7915e7b5da5947e05f9c15103bc70932c8bad1
yezy <yezileliilomo@hotmail.com>
2020-06-24 16:58:15 +0300

    ✨Make InfoModal scrollable

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -10,7 +10,7 @@ function InfoModal(props) {
     return (
         <Modal className="info-modal" animation={false} dialogClassName="cusom-modal-dialog"
             show={props.modalShow} onHide={() => props.setModalShow(false)} size="lg"
-            aria-labelledby="contained-modal-title-vcenter" centered>
+            aria-labelledby="contained-modal-title-vcenter" scrollable centered>
             <Modal.Header className="modal-header p-2 pt-3 bg-light" closeButton>
                 <Modal.Title>
                     <h4 class="modal-title">{props.header}</h4>
